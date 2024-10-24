4b2b1afc32f357e3c9bf7b89e49ae752b224cd2d
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-10-24 16:46:54 +0200

    add policy for ssm to lambda

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from bot import app
 
 from handlers.schedule import handle_schedule
@@ -6,9 +7,9 @@ from handlers.webhook import handle_webhook
 def lambda_handler(event, context):
     match event:
         case {'httpMethod': "POST", 'path': '/webhook'}:
-            return handle_webhook(event)
-        case {'source': 'aws.events'}:
-            return handle_schedule(event)
+            return asyncio.get_event_loop().run_until_complete(handle_webhook(event))
+        case {'source': 'aws.schedule'}:
+            return asyncio.get_event_loop().run_until_complete(handle_schedule(event))
 
 
 
